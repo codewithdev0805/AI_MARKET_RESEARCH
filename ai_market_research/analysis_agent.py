5db1eda168c942da02cce09c65f167d3b3e60622
codewithdev0805 <codewithdev0805@gmail.com>
2025-08-12 00:20:07 +0530

    D

--- ai_market_research/analysis_agent.py
+++ ai_market_research/analysis_agent.py
@@ -28,7 +28,7 @@ def run_analysis_agent(keyword):
     """
 
     completion = client.chat.completions.create(
-        model="llama-3.1-70b-versatile",
+        model="llama-3.3-70b-versatile",
         messages=[{"role": "user", "content": prompt}]
     )
 
